174f3fa5c99df7c61ad2e9ed6df623f302bc9038
SarwanGuna <saravanan3988@hotmail.com>
2019-01-24 15:50:31 +0530

    First git

---
+++ src/org/test/git/Testing.java
@@ -0,0 +1,13 @@
+package org.test.git;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		testing();
+	}
+ public static void testing() {
+	
+	System.out.println("Testing the git");
+}
+}
